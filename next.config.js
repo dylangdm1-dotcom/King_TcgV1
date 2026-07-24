5ea2b30570dd282369dfd2d253fb571903a22bc2
dylangdm1-dotcom <dylangdm1@gmail.com>
2026-07-24 19:38:41 +0200

    Add webpack fallback configuration for client builds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
