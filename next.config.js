6d37c5ed86f3ed82f60cd30a169d7e824e516044
dylangdm1-dotcom <dylangdm1@gmail.com>
2026-08-26 17:25:51 +0200

    Remove eslint ignoreDuringBuilds from next config
    Removed eslint ignoreDuringBuilds option from configuration.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   webpack: (config, { isServer }) => {
     // Résolution des modules optionnels côté client
     if (!isServer) {
